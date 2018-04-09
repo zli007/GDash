d32da4e1f43c1d97299d3e888ac08753c541ab0e
zli007 <38192974+zli007@users.noreply.github.com>
2018-04-08 21:58:01 -0500

    Add files via upload

---
+++ OS/OShg19/view.js
@@ -0,0 +1,5 @@
+ 
+var chrId = 22,
+    default_viewStart = 30014044, 
+    default_viewEnd = 30017798,
+    diff = 174; 
---
+++ OS/OShg38/view.js
@@ -0,0 +1,5 @@
+ 
+var chrId = 22,
+    default_viewStart = 30014047, 
+    default_viewEnd = 30015936 
+    diff = 80;
